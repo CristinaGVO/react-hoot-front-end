96e7238eff16a142094324973955fb15a36acd32
CristinaGVO <vargasosoriocristina@gmail.com>
2026-01-16 16:31:29 -0800

    lab

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,36 @@
+import { useContext, useEffect, useState } from 'react';
+
+import { UserContext } from '../../contexts/UserContext';
+import * as userService from '../../services/userService';
+
+
+const Dashboard = () => {
+  const { user } = useContext(UserContext);
+  const [users,setUsers] = useState([])
+
+  useEffect(()=>{
+    const fetchUsers = async()=>{
+        try{
+            const fetchedUsers = await userService.index()
+            setUsers(fetchedUsers);
+        }catch(err){
+            console.log(err)
+        }
+    }
+    if (user) fetchUsers();
+  }, [user])
+
+  return (
+    <main>
+      <h1>Welcome, {user.username}</h1>
+      {users.map((user)=>(
+        <p key={user._id}> {user.username}</p>
+      ))}
+    </main>
+  );
+};
+
+export default Dashboard;
+
+
+
