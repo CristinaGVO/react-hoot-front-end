56d7315a196e1ad3275cc172863410b94c193bf9
CristinaGVO <vargasosoriocristina@gmail.com>
2026-01-16 16:31:29 -0800

    lab

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,48 @@
+
+import { useContext } from 'react';
+import { Link } from 'react-router';
+
+import { UserContext } from '../../contexts/UserContext';
+
+const NavBar = () => {
+  // Get the setUser function from the UserContext
+  const { user, setUser } = useContext(UserContext);
+
+  // Add the handleSignOut function
+  const handleSignOut = () => {
+    // Clear the token from localStorage
+    localStorage.removeItem('token');
+    // Clear the user state
+    setUser(null);
+  };
+
+
+
+
+  return (
+    <nav>
+      {user ? (
+        <ul>
+          <li><Link to='/'>HOME</Link></li>
+          <li><Link to='/hoots'>HOOTS</Link></li>
+          {/* Add the NEW HOOT link */}
+          <li><Link to='/hoots/new'>NEW HOOT</Link></li>
+          <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
+        </ul>
+      ) : (
+        <ul>
+          <li><Link to='/'>HOME</Link></li>
+          <li><Link to='/sign-in'>SIGN IN</Link></li>
+          <li><Link to='/sign-up'>SIGN UP</Link></li>
+        </ul>
+      )}
+    </nav>
+  );
+
+
+};
+
+
+export default NavBar;
+
+
